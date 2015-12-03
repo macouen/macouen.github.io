71c29c5d1816904dff80a9701646adc15d49e4e2
zhaominmin <zhaominmin@aprbrother.com>
2015-12-03 11:15:26 +0800

    Site updated: 2015-12-03 11:15:23

--- js/hook-duoshuo.js
+++ js/hook-duoshuo.js
@@ -1,7 +1,7 @@
 if (typeof DUOSHUO !== 'undefined') {
 	hook_duoshuo_templates();
 } else {
-	$('#duoshuo-script')[0].onload = hook_duoshuo_templates;
+	$('[src="http://static.duoshuo.com/embed.js"]')[0].onload = hook_duoshuo_templates;
 }
 var is_hook_duoshuo = false;
 
@@ -17,11 +17,7 @@ function hook_duoshuo_templates() {
 		var is_admin;
 		if (typeof duoshuo_user_ID !== 'undefined') {
 			if (e.post.author.user_id && (e.post.author.user_id == duoshuo_user_ID)) {
-				if(duoshuo_admin_nickname){
-				is_admin = '<span class="this_ua admin">'+duoshuo_admin_nickname+'</span>'
-				}else{
-					is_admin = '<span class="this_ua admin">博主</span>'
-				}
+				is_admin = '<span class="this_ua admin">博主</span>'
 			} else {
 				is_admin = '';
 			}
